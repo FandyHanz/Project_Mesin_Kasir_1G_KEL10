a761524b323e4d142bd4b7a7c49c2ce0c3d58940
Fadlihh <62737219+Fadlihh@users.noreply.github.com>
2023-09-15 20:02:21 +0700

    First unfinish

--- PROJECT_MESIN_KASIR/src/App.java
+++ PROJECT_MESIN_KASIR/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        int harga [] []={25_000,30_000,35_000},{5000};
+       Scanner user = new Scanner(system.in);
+        String makanan,minuman;
+        double jml_diskon =0,5 ,diskon,harga;
+        System.out.Println("|===================================|\n
+	                        |Selamat Datang di Resto Bunga Harum|\n
+	                        |===================================|\n
+	                        |berikut ini adalah list menu kami: |\n
+                            |Nasi Goreng		: 12,500    |\n
+	                        |Ikan Goreng		: 10,000    |\n
+                            |Ayam Goreng Utuh 	: 45,000    |\n
+                            |				    |\n
+	                        |Minuman: 			    |\n
+                            |Teh Tawar 		: 5,000     |\n
+                            |===================================| \n
+                        										\n");
+        System.out.println("Selamat datang silahkan masukan makanan yang anda mau \n");
     }
 }
