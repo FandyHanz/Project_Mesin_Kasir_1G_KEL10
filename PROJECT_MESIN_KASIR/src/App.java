0588ccfdafc703598ab9b8683bc09885c833d7d2
FandyHanz <hanzyura28@gmail.com>
2023-09-16 10:20:09 +0700

    pembenahan

--- PROJECT_MESIN_KASIR/src/App.java
+++ PROJECT_MESIN_KASIR/src/App.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args){
-        int harga [] []={25_000,30_000,35_000},{5000};
-       Scanner user = new Scanner(system.in);
-        String 
-        System.out.Println("|===================================|\n
-	                        |Selamat Datang di Resto Bunga Harum|\n
-	                        |===================================|\n
-	                        |berikut ini adalah list menu kami: |\n
-                            |Nasi Goreng		: 12,500    |\n
-	                        |Ikan Goreng		: 10,000    |\n
-                            |Ayam Goreng Utuh 	: 45,000    |\n
-                            |				    |\n
-	                        |Minuman: 			    |\n
-                            |Teh Tawar 		: 5,000     |\n
-                            |===================================| \n
-                        										\n");
+        double harga []={12500,10000,45000,5000};
+        Scanner input = new Scanner(System.in);
+
+        String barang,minuman,ayam_goreng,ikan_goreng,nasi_goreng,teh_tawar;
+        int jumlahBarang,totalHarga;
+        double diskonDadakan = 0.5,hargaFinal;
+        System.out.println("|===================================|");
+	    System.out.println("|Selamat Datang di Resto Bunga Harum|");
+	    System.out.println("|===================================|");
+	    System.out.println("|berikut ini adalah list menu kami: |");
+        System.out.println("|Nasi Goreng		: 12,500          |");
+	    System.out.println("|Ikan Goreng		: 10,000          |");
+        System.out.println("|Ayam Goreng Utuh 	: 45,000      |");
+        System.out.println("|				                      |");
+	    System.out.println("|Minuman: 			              |");
+        System.out.println("|Teh Tawar 		    : 5,000       |");
+        System.out.println("|===================================|");
         System.out.println("Selamat datang silahkan masukan makanan yang anda mau");
-        
+        barang = input.nextLine();
+        System.out.println("silahkan masukan minuman yang anda mau");
+        minuman = input.nextLine();
+        if (minuman=="teh_tawar"){
+            System.out.println(harga[3]);
+        }
+        input.close();
     }
 }
+
---
+++ PROJECT_MESIN_KASIR/src/kasir.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main(){
+	std::string a,b;
+	double harga[]={12500, 10000, 45000, 5000};
+	
+	std::cout<<"	|===================================|\n";
+	std::cout<<"	|Selamat Datang di Resto Bunga Harum|\n";
+	std::cout<<"	|===================================|\n";
+	std::cout<<"	|berikut ini adalah list menu kami: |\n";
+	std::cout<<"	|Nasi Goreng		: 12,500    |\n";
+	std::cout<<"	|Ikan Goreng		: 10,000    |\n";
+	std::cout<<"	|Ayam Goreng Utuh 	: 45,000    |\n";
+	std::cout<<"	|				    |\n";
+	std::cout<<"	|Minuman: 			    |\n";
+	std::cout<<"	|Teh Tawar 		: 5,000     |\n";
+	std::cout<<"	|===================================| \n";
+	std::cout<<"											\n";
+	std::cout<<" ingin pesan apa: \n";
+	std::cin>> a;
+	if( a=="nasi_goreng"){
+		std::cout<<"harga : 	"<<harga[0] ; 
+	}
+	else if(a=="ikan_goreng"){
+		std::cout<<"harga :		"<<harga[1];
+	}
+	else if(a=="ayam_goreng"){
+		std::cout<<"harga :		"<<harga[2];	
+	}
+	std::cout<<"								\n";
+	std::cout<<"Untuk minum nya mau pesan apa:	\n";
+	std::cin>>b;
+	if(b=="teh_tawar"){
+		std::cout<<"harga :		"<<harga[3];
+	}
+	else{
+		std::cout<<"maaf, stok benda tu kosong";
+	}
+	
+     
+
+	
+}
