a2ec0a4bb22477d02b8ce35f1dbece50089225e1
Fadlihh <62737219+Fadlihh@users.noreply.github.com>
2023-09-18 21:12:50 +0700

    Progres 45%

--- PROJECT_MESIN_KASIR/src/App.java
+++ PROJECT_MESIN_KASIR/src/App.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 class harga{
     int pilihanMakan,pilihanMinum;
      int hargaTot;
-    public harga(double pilihanMakan2, double pilihanMinum2) {
+    public harga(double pilihanMakan, double pilihanMinum) {
     }
-    public void addNumbers(double piMak,double piMin){
+    public void addNumbers(int piMak,int piMin){
         this.pilihanMakan=(int)piMak;
         this.pilihanMinum=(int)piMin;
     }
@@ -20,12 +20,14 @@ class harga{
         double[] harga={12500,10000,45000,5000};
 
         Scanner input = new Scanner(System.in);
+        App obj= new App();
+        harga obj1=new harga();
 
-        harga obj=new harga(pilihanMakan,pilihanMinum);
-        int jumlahBarang,totalHarga;
-        double diskonDadakan = 0.5;
+
+        int jumlahBarang,totalHarga,hargaFix,nominal,exchange;
+        double diskon = 0.05;
         System.out.println("|===================================|");
-	    System.out.println("|Selamat Datang di Resto Bunga Harum|");
+	    System.out.println("|Selamat Datang di Resto Cin Cin Men|");
 	    System.out.println("|===================================|");
 	    System.out.println("|berikut ini adalah list menu kami: |");
         System.out.println("|Nasi Goreng		: 12,500           |");
@@ -59,7 +61,40 @@ class harga{
         System.out.println(harga[3]);
         pilihanMinum = 3;
         }
-        System.out.println("total"+ obj.solve());
+        System.out.println("Adakah tambahan pesanan lagi: ");
+        String barang2=input.nextLine();
+
+         if(barang.equals("nasi goreng")){
+            System.out.println(harga[0]);
+            pilihanMakan = 0;
+        }
+        else if(barang.equals("ikan goreng")){
+            System.out.println(harga[1]);
+            pilihanMakan = 1;
+        }
+        else if(barang.equals("ayam goreng utuh")){
+            System.out.println(harga[2]);
+            pilihanMakan = 2;
+        }
+        else{
+            System.out.println("Maaf, kami tidak menjual barang itu");
+        }
+         if (minuman.equals("teh tawar")){
+        System.out.println(harga[3]);
+        pilihanMinum = 3;
+        }
+        System.out.println("total"+ obj1.solve());
+        
+        if(totalHarga<=25000){
+            System.out.println("selamat anda mendapatakan diskon sebesar 5%, menjadi: ");
+            hargaFix=totalHarga-(totalHarga*(int)0.05);
+            System.out.println(hargaFix);
+        }
+        System.out.println("masukan nominal pembayaran: ");
+        nominal = input.nextInt(){
+
+        }
+        
         input.close();
     }
 }
