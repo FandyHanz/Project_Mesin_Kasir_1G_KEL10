0f677054eae17e8e2c2411eb492d7f69b80b0604
Fadlihh <62737219+Fadlihh@users.noreply.github.com>
2023-09-22 19:29:55 +0700

    pembenahan 2

--- PROJECT_MESIN_KASIR/src/MesinKasir.java
+++ PROJECT_MESIN_KASIR/src/MesinKasir.java
@@ -1,24 +1,34 @@
 import java.util.Scanner;
 public class MesinKasir {
     public static void main(String[] args){
-        int[] harga = {12000,11000,10000,5000};
-        Scanner user = new Scanner(System.in); 
-        String masukanMakanan ,masukanMinuman;
-        int makanan,minuman;
-        double diskon = 0.05;
-        System.out.println("========================= \n");
-        System.out.println("List makanan dan minuman: \n");
-        System.out.println(" . Nasi goreng \n");
-        System.out.println("2. Bakmie goreng \n");
-        System.out.println("3. Soto         \n");
-        System.out.println("Minuman:        \n");
-        System.out.println("1. Soda Gembira \n");
-        System.out.println("========================= \n");
-        System.out.println("\n");
-        System.out.println("Mau pesan apa: ");
-        masukanMakanan =user.nextLine();
-        if(masukanMakanan.equals("nasi goreng")){
+        Scanner user = new Scanner(System.in);
+        Scanner user1 = new Scanner(System.in);
 
+        String makanan,minuman,revisi;
+        int hargaMakan,hargaMinum,total;     
+        double diskon = 0.05;
+        System.out.println("=========================");
+        System.out.println("List makanan dan minuman:");
+        System.out.println("1. Nasi goreng      45.000");
+        System.out.println("2. Bakmie goreng    35.000");
+        System.out.println("3. Soto             20.000");
+        System.out.println("Minuman:                   ");
+        System.out.println("1. Soda Gembira     10.000");
+        System.out.println("=========================");
+        System.out.println("\n");
+        System.out.println("Mau pesan apa makanan:  ");
+        makanan = user1.nextLine();
+        System.out.println("harga: ");
+        hargaMakan = user.nextInt();
+        System.out.println("Masukan Minuman: ");
+        minuman = user1.nextLine();
+        System.out.println("harga: ");
+        hargaMinum = user.nextInt();
+        System.out.println("Apakah ada catatan opsional/revisi: ");
+        revisi = user1.nextLine();
+        total = hargaMakan+hargaMinum;
+        System.out.println("Total makanan anda adalah: "+makanan+" dan "+ minuman + " dengan harga Rp." + total);
+        System.out.println("Dengan catatan opsional: " + revisi);
+        user.close();
         }
     }
-}
