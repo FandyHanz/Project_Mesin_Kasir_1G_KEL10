226b90d79930f1b8ef9a572c8ba8d4876ac09adf
Fadlihh <62737219+Fadlihh@users.noreply.github.com>
2023-09-22 07:55:27 +0700

    Belum Fix

---
+++ PROJECT_MESIN_KASIR/src/MesinKasir.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.io.File;
+public class oretoretan {
+    public static void main(String[] args){
+        int[] harga = {12000,11000,10000,5000};
+        Scanner user = new Scanner(System.in);
+        File file = 
+        String masukanMakanan,masukanMinumaString;
+        int makanan,minuman;
+        double diskon = 0.05;
+        System.out.println("========================= \n");
+        System.out.println("List makanan dan minuman: \n");
+        System.out.println("1. Nasi goreng \n");
+        System.out.println("2. Bakmie goreng \n");
+        System.out.println("3. Soto         \n");
+        System.out.println("Minuman:        \n");
+        System.out.println("1. Soda Gembira \n");
+        System.out.println("========================= \n");
+        System.out.println("\n");
+        System.out.println("Mau pesan apa: ");
+        masukanMakanan=user.nextLine();
+        if(masukanMakanan.equals("nasi goreng")){
+
+        }
+    }
+}
