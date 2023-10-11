b4be4efad4bc1e4928aeba01d9edf00db5325e47
FandyHanz <hanzyura28@gmail.com>
2023-10-11 09:45:53 +0700

    Menghitung jumlah pesanan

---
+++ PROJECT_MESIN_KASIR/src/Fitur/Menghitungjumlah.java
@@ -0,0 +1,12 @@
+package Fitur;
+
+class Menghitungjumlah {
+     static void solver(int makanan,int minuman,int qty){
+        int hargaMakanan=makanan*qty;
+        int hargaMinuman=minuman*qty;
+        int Total=hargaMakanan+hargaMinuman;
+    }
+}
+
+
+    
