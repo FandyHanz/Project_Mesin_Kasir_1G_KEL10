aa45e15088a4584ebbd730f640f989f81d42986f
Afqufai <143379198+Afqufai@users.noreply.github.com>
2023-10-16 21:14:25 +0700

    Bilingual Revision & Login Page Conditions

--- PROJECT_MESIN_KASIR/src/Fitur/Bilingual.java
+++ PROJECT_MESIN_KASIR/src/Fitur/Bilingual.java
@@ -10,10 +10,16 @@ public class Bilingual {
         langSelect = inputL.nextLine();
         if (langSelect.equalsIgnoreCase("en")){
             System.out.println("English Language Selected");
+            System.out.println("Are you the cashier? (y/n, yes/no)");
+            //Program dilanjutkan dalam bahasa inggris...
+
         } else if (langSelect.equalsIgnoreCase("id")){
             System.out.println("Bahasa Indonesia terpilih");
+            System.out.println("Apakah anda kasirnya? (y/t, ya, tidak)");
+            //Program dilanjutkan dalam bahasa indonesia...
         } else {
             System.out.println("INPUT BAHASA TIDAK VALID | LANGUAGE INPUT INVALID");
         }
+        inputL.close();
     }
 }
--- PROJECT_MESIN_KASIR/src/Fitur/MultiPengguna.java
+++ PROJECT_MESIN_KASIR/src/Fitur/MultiPengguna.java
@@ -4,28 +4,38 @@ public class MultiPengguna {
     public static void main(String[] args) {
         Scanner user1 = new Scanner (System.in) ;
         Scanner password = new Scanner(System.in);
-        String ps,usr,Pass1,Pass2,id1,id2;
+        String ps,usr,Pass1,Pass2,adminpw,id1,id2,admin;
         id1="Fadlih";
         id2="Afrizal";
+        admin="Admin";
         Pass1="Donat";
         Pass2="Udahkuduga";
-        System.out.println("Masukkan ID Pengguna: ");
+        adminpw = "bestAdmin";
+        System.out.print("Masukkan ID Pengguna: ");
         usr=user1.nextLine();
-        System.out.println("Masukkan Password");
+        System.out.print("Masukkan Password: ");
         ps=password.nextLine();
-        if (usr.equals(id1)) {
-            System.out.println("halo "+id1);
+
+        if ((usr.equals(id1) && ps.equals(Pass1))|| (usr.equals(id2) && ps.equals(Pass2)) || (usr.equals(admin) && ps.equals(adminpw))){
+            if (usr.equals(admin) && ps.equals(adminpw)){
+                System.out.println("Login sebagai Admin berhasil!");
+                System.out.println("Selamat datang kembali, administrator");
+                System.out.println("Apa yang ingin anda lakukan hari ini?");
+                System.out.println("[1] Setup diskon, [2] Restock");
+                //Lanjutkan Tampilan Admin
+            } else {
+                System.out.println("Login sebagai kasir berhasil!");
+                System.out.println("Selamat datang kembali"+usr);
+                System.out.println("Apa yang ingin anda lakukan hari ini?");
+                System.out.println("[1] Input diskon, [2] Layani Pelanggan");
+                //Lanjutkan Tampilan kasir
+            }
+        } else {
+            System.out.println("DENIED! USERNAME DAN PASSWORD SALAH");
+            System.out.println("Login gagal! Mohon cek kembali username dan password");
         }
-        else if (usr.equals(id2)){
-            System.out.println("halo "+id2);
-        }
-        if (password.equals(Pass1)){
-            System.out.println("Login berhasil");
-        }
-        else if (password.equals(Pass2)){
-            System.out.println("Login berhasil");
-            password.close();
-            user1.close();
-        }
+
+        user1.close();
+        password.close();
     }
 }
